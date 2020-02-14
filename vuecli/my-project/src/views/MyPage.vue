378eb0c783ab599791d9dc5ad410e8ea9381aad9
lyt2045817743 <2045817743@qq.com>
2020-02-14 17:49:30 +0800

    vuecli

---
+++ vuecli/my-project/src/views/MyPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>这是我的vue页面！</h1>
+        {{str}}
+    </div>    
+</template>
+<script>
+export default {
+    data(){
+        return { 
+            str:"fellow37"
+        }
+    },
+    
+}
+</script>
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
