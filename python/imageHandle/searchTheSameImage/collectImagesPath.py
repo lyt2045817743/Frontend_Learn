d7a096913c635b13d12e4c0b08506a8e6f237f53
lyt2045817743 <liuyuting7@xiaomi.com>
2023-08-18 17:01:28 +0800

    添加注释

--- python/imageHandle/searchTheSameImage/collectImagesPath.py
+++ python/imageHandle/searchTheSameImage/collectImagesPath.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# 功能：收集一个/多个目标文件夹的所有图片路径，并统一输出到txt文件
 import os
 import sys
 
@@ -5,14 +7,17 @@ args = sys.argv
 script_name = args[0]
 parameters = args[1:]
 
+# 支持多个目标文件夹
 directory_pathArr = parameters
 
 image_extensions = ['.jpg', '.jpeg', '.png', '.gif']
 
+# 输出所有图片路径的文件名称
 output_filename = 'imagePaths.txt'
 
-print("please wait...")
+print("🚩 开始收集，稍等一下...")
 
+# 收集目标文件夹图片路径
 image_paths = []
 for directory_path in directory_pathArr:
     for root, _, files in os.walk(directory_path):
@@ -21,8 +26,9 @@ for directory_path in directory_pathArr:
             if ext.lower() in image_extensions:
                 image_paths.append(os.path.join(root, file))
 
+# 写入到txt文件
 with open(output_filename, 'w') as output_file:
     for path in image_paths:
         output_file.write(path + '\n')
 
-print("success! The file for storing the image path is ready: {}".format(output_filename))
+print("✅ 成功啦! 收集图片路径的文件已准备好: {}".format(output_filename))
--- python/imageHandle/searchTheSameImage/searchTheSameImages.py
+++ python/imageHandle/searchTheSameImage/searchTheSameImages.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# 功能：将imagePaths.txt中所有的图片和目标图片进行比对
+# 输出相似的图片。可输入配置不同的阙值，阙值越大，输出的图片与目标图片相似度越小
 from PIL import Image
 import imagehash
 import sys
@@ -7,21 +10,26 @@ args = sys.argv
 script_name = args[0]
 parameters = args[1:]
 
+# 需要比对的目标图片路径
 given_image_path = parameters[0]
 similarity_threshold = 0
 
+# 如果用户配置了阙值，则根据输入重置
 if len(parameters) == 2:
     similarity_threshold = int(parameters[1])
 
-print("please wait...")
+print("🚩 开始比对图片内容，稍等一下...\n")
 
+# 读取收集好的所有图片路径
 with open('imagePaths.txt', 'r') as file:
     image_paths = file.readlines()
 
 image_paths_array = [path.strip() for path in image_paths]
 
+# 目标图片的hash
 given_hash = imagehash.phash(Image.open(given_image_path))
 
+# 将所有图片与目标图片进行Hash值比较，并根据设置的阙值判断是否输出到结果集中
 similarities = []
 for path in image_paths_array:
     try:
@@ -31,7 +39,7 @@ for path in image_paths_array:
         if similarity <= similarity_threshold:
             similarities.append(path)
     except Exception as e:
-        print("An error occurred while trying to open {}: {} error".format(path, e))
+        print("\n❗️ 部分图片解析失败 {}: {} error\n".format(path, e))
 
-print('job success...')
+print('✅ 成功啦！以下是相似的图片路径集合')
 print(similarities)
