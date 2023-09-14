2771eea6b2060def24ea275f1ae6c30059825c00
lyt2045817743 <liuyuting7@xiaomi.com>
2023-09-14 16:49:06 +0800

    feat: 手写promise的6个静态方法

--- js/promise/myPromise.js
+++ js/promise/myPromise.js
@@ -6,7 +6,7 @@ function MyPromise(executor) {
     if (_this.status === 'pending') {
       _this.status = 'fulfilled';
       _this.result = data;
-      handlerExe();
+      handlerAllExe();
     }
   }
 
@@ -54,7 +54,7 @@ function MyPromise(executor) {
   }
 
   this.then = function(onFulfilled, onRejected) {
-    return new Promise((resolve, reject) => {
+    return new MyPromise((resolve, reject) => {
       if (_this.status !== 'pending') {
         handlerEachExe({ onFulfilled, onRejected, resolve, reject });
       } else {
@@ -73,6 +73,93 @@ function MyPromise(executor) {
   }
 }
 
+MyPromise.resolve = function(value) {
+  // 这个判断很重要，不然会将promise对象作为数据直接传给then回调
+  if (value instanceof MyPromise) {
+    return value;
+  }
+  return new MyPromise(resolve => resolve(value));
+}
+
+MyPromise.reject = function(err) {
+  if (value instanceof MyPromise) {
+    return value;
+  }
+  return new MyPromise((_resolve, reject) => reject(err));
+}
+
+MyPromise.all = function(promises) {
+  return new Promise((resolve, reject) => {
+    const result = [];
+    for (let i = 0; i < promises.length; i++) {
+      const item = promises[i];
+      MyPromise.resolve(item).then((data) => {
+        result[i] = data;
+        // resolve和reject的调用都应该在then或catch事件处理函数内，否则就是同步改变状态了
+        if (result.length === promises.length && !result.includes(undefined)) {
+          resolve(result);
+        }
+      }).catch((err) => {
+        reject(err);
+      })
+    }
+  })
+}
+
+MyPromise.race = function(promises) {
+  return new MyPromise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      MyPromise.resolve(promises[i]).then(resolve).catch(reject);
+    }
+  })
+}
+
+MyPromise.any = function(promises) {
+  return new MyPromise((resolve, reject) => {
+    const reasons = [];
+    for (let i = 0; i < promises.length; i++) {
+      const item = promises[i];
+      MyPromise.resolve(item).then(resolve).catch(error => {
+        reasons[i] = error;
+        if (reasons.length === promises.length && !reasons.includes(undefined)) {
+          try {
+            reject(new AggregateError(reasons)); // node版本过低的话则报ReferenceError异常
+          } catch (error) {
+            if (error instanceof ReferenceError) {
+              reject(reasons);
+            }
+          }
+        }
+      })
+    }
+  })
+}
+
+MyPromise.allSettled = function (promises) {
+  const onFulfilled = data => ({ status: 'fulfilled', data });
+  const onRejected = reason => ({ status: 'rejected', reason });
+  return MyPromise.all(promises.map(item => MyPromise.resolve(item).then(onFulfilled, onRejected)));
+}
+
+MyPromise.allSettled2 = function(promises) {
+  return new MyPromise((resolve) => {
+    const result = [];
+    for (let i = 0; i < promises.length; i++) {
+      MyPromise.resolve(promises[i]).then((value) => {
+        result[i] = { status: 'fulfilled', value };
+        if (result.length === promises.length && !result.includes(undefined)) {
+          resolve(result);
+        }
+      }).catch((reason) => {
+        result[i] = { status: 'rejected', reason };
+        if (result.length === promises.length && !result.includes(undefined)) {
+          resolve(result);
+        }
+      })
+    }
+  })
+}
+
 const p = new MyPromise((resolve, reject) => {
   resolve('success');
   // reject('error');
@@ -80,9 +167,52 @@ const p = new MyPromise((resolve, reject) => {
 
 p.then((data) => {
   console.log(data);
-  fun()
+  // fun()
 }, (error) => {
   console.log('then onRejected: ', error);
 }).catch((error) => {
   console.log('next promise: ', error);
+})
+
+const p2 = MyPromise.all([
+  new MyPromise(resolve => setTimeout(() => resolve(1), 300)),
+  new MyPromise(resolve => setTimeout(() => resolve(2), 200)),
+  3,
+  // new MyPromise((_resolve, reject) => setTimeout(() => reject(new Error('error')), 100)),
+]).then((data) => {
+  console.log('promise all data: ', data);
+}).catch((err) => {
+  console.log('promise all err: ', err);
+})
+
+// 或allSettled2
+MyPromise.allSettled([
+  new MyPromise(resolve => setTimeout(() => resolve(1), 300)),
+  new MyPromise(resolve => setTimeout(() => resolve(2), 200)),
+  new MyPromise((_resolve, reject) => setTimeout(() => reject(new Error('error')), 100)),
+]).then((data) => {
+  console.log('promise allSettled data: ', data);
+})
+
+MyPromise.race([
+  new MyPromise(resolve => setTimeout(() => resolve(1), 300)),
+  new MyPromise(resolve => setTimeout(() => resolve(2), 200)),
+  new MyPromise((_resolve, reject) => setTimeout(() => reject(new Error('error')), 100)),
+]).then((data) => {
+  console.log('promise race data: ', data);
+}).catch(err => {
+  console.log('promise race error: ', err);
+})
+
+MyPromise.any([
+  // new MyPromise(resolve => setTimeout(() => resolve(1), 300)),
+  // new MyPromise(resolve => setTimeout(() => resolve(2), 200)),
+  new MyPromise((_resolve, reject) => setTimeout(() => reject(new Error('error 50')), 50)),
+  new MyPromise((_resolve, reject) => setTimeout(() => reject(new Error('error 200')), 200)),
+  new MyPromise((_resolve, reject) => setTimeout(() => reject(new Error('error 100')), 100)),
+]).then((data) => {
+  console.log('promise any data: ', data);
+}).catch(err => {
+  console.log('promise any error constructor: ', err.constructor.name);
+  console.log('promise any error: ', err);
 })
